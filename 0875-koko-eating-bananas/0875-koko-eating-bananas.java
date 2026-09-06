c7ea938cfbc471bedd8f657dfee7e33ff31c9c0c
HimanshuRa0 <himanshurao00515@gmail.com>
2026-09-06 09:21:24 +0530

    Time: 23 ms (22.44%), Space: 48 MB (43.47%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -7,7 +7,7 @@ class Solution {
         }
         while (left < right) {
             int mid = left + (right - left) / 2;
-            int hoursNeeded = 0; 
+            long hoursNeeded = 0; 
             for (int pile : piles) {
                 hoursNeeded += (int) Math.ceil((double) pile / mid); 
             }
